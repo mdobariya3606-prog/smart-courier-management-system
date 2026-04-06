cffec2cdce875ccbe789d6cea40a698e377e2940
MEET <mdobariya3606@gmail.com>
2026-04-06 11:36:08 +0530

    Initial Commit

---
+++ src/main/java/com/smart_courier_management_system/smart_courier_management_system/SmartCourierManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smart_courier_management_system.smart_courier_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCourierManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCourierManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_courier_management_system/smart_courier_management_system/SmartCourierManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_courier_management_system.smart_courier_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCourierManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
